c6a72e2c713acf3eb6447357b842c72d48847621
haidong <haidong.ji@gmail.com>
2013-04-10 13:36:03 -0700

    Create win2012DomainADInstall.ps1
    GUI generated script during Windows 2012 Domain and Active Directory service install.

---
+++ win2012DomainADInstall.ps1
@@ -0,0 +1,18 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2008R2" `
+-DomainName "HaidongWorks.local" `
+-DomainNetbiosName "HAIDONGWORKS" `
+-ForestMode "Win2008R2" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
