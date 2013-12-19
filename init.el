14cf93b2e7462fb8ce1e9848448a85f47c04215f
haidong <haidong.ji@gmail.com>
2013-12-18 19:14:46 -0600

    Added org-pomodoro and its configurations
    1. Uses sounder.exe from http://www.elifulkerson.com/projects/commandline-wav-player.php for playing sound on Windows.
    
    Remember to set it to "aplay" on Linux.
    
    2. Created local keybinding F12 to start a pomodoro;
    
    3. Created local keybinding to toggle tick-playing.

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 ;; ---------------------------
 ;; -- Install packages I need
 ;; ---------------------------
-(defvar my-packages '(evil smart-tab yasnippet)
+(defvar my-packages '(evil smart-tab yasnippet org-pomodoro)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -37,6 +37,22 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 (setq org-agenda-files (list "life.org"))
+
+(require 'org-pomodoro)
+(setq org-pomodoro-audio-player "sounder.exe")
+(setq org-pomodoro-play-sounds t)
+(defun toggle-pomodoro-ticking-sound ()
+  (interactive)
+  (if org-pomodoro-play-ticking-sounds
+    (setq org-pomodoro-play-ticking-sounds 'nil)
+    (setq org-pomodoro-play-ticking-sounds t)))
+(defun mp-add-pomodoro-ticking-toggle ()
+  (local-set-key (kbd "<f11>") 'toggle-pomodoro-ticking-sound))
+(defun mp-add-pomodoro-start-key ()
+  (local-set-key (kbd "<f12>") 'org-pomodoro))
+(add-hook 'org-mode-hook 'mp-add-pomodoro-start-key)
+(add-hook 'org-mode-hook 'mp-add-pomodoro-ticking-toggle)
+
 
 (add-to-list 'hippie-expand-try-functions-list
 'yas/hippie-try-expand) ;put yasnippet in hippie-expansion list
