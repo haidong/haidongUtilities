69c8fc31c0ccf2e7495f2f7f9bf25e0a572f8965
Haidong Ji <ji@haidongji.com>
2022-01-05 14:48:00 -0600

    added magit, also column width in status line

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
     ;; make evil-search-word look for symbol rather than word boundaries
     (setq-default evil-symbol-word-search t))
 
-
+(setq column-number-mode t)
 ;; org config
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -66,7 +66,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil))))
+ '(package-selected-packages '(format-all magit evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
