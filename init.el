07dcfb497a66eb04795eaa73c1e610850b45a903
Haidong Ji <ji@haidongji.com>
2022-06-03 08:43:05 -0500

    Added Java 3rd party jump function

--- init.el
+++ init.el
@@ -72,6 +72,7 @@
 (use-package corfu-info)
 (use-package corfu-history)
 (require 'lsp-bridge)
+(require 'lsp-bridge-jdtls)       ;; provide Java third-party library jump and -data directory support, optional
 (setq lsp-bridge-completion-provider 'corfu)
 (require 'lsp-bridge-icon)
 (require 'lsp-bridge-orderless)
