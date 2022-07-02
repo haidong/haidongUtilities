bd8da3d689ea285869a31dbb0b64e65c3cc32bf7
Haidong Ji <ji@haidongji.com>
2022-07-02 15:25:08 -0500

    Slight directory adjustment

--- init.el
+++ init.el
@@ -88,14 +88,14 @@
 (add-hook 'c++-mode-hook 'outline-minor-mode)
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cf" 'format-all-buffer)
-(find-file "~/cloud/HaidongPlanning/ToDo.org")
+(find-file "~/cloud/orgmode/ToDo.org")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/cloud/HaidongPlanning/ToDo.org"))
+ '(org-agenda-files '("~/cloud/orgmode/ToDo.org"))
  '(package-selected-packages '(org-roam evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
